4fd08954d0c74bbea8ef77a365b50b61ea23c61c
Tirian33 <tirianjudy@gmail.com>
2024-04-16 18:21:56 -0500

    Asteroid sprites added. SFX for movement. added collision-demonstration scene.

--- player.gd
+++ player.gd
@@ -3,11 +3,14 @@ extends Area2D
 signal death
 
 @onready var spaceship = $Spaceship
+@onready var sfx = $thruster
 @export var cust_grav = 250
 @export var rotateSpeed = 90
 
 var spaceshipDown = deg_to_rad(120)
 var spaceshipUp = deg_to_rad(60)
+var playing_sound = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,16 +23,25 @@ func _process(delta):
 		spaceship.animation = "on"
 		if spaceship.rotation >= spaceshipUp:
 			spaceship.rotation -= deg_to_rad(rotateSpeed) * delta
+		
+		if not playing_sound:
+			sfx.play()
+			playing_sound = true
+		
 	else:
 		spaceship.animation = "off"
 		if spaceship.rotation <= spaceshipDown:
 			spaceship.rotation += deg_to_rad(rotateSpeed) * delta
+		
+		if playing_sound:
+			sfx.stop()
+			playing_sound = false
 
 	var pointing = ((rad_to_deg(spaceship.rotation) - 90) / 30)
 	spaceship.position += Vector2.DOWN * cust_grav * delta * pointing
 	
 	if spaceship.position.y < -1 or spaceship.position.y > 649:
-		print_debug("death")
+		#print_debug("death")
 		emit_signal("death")
 
 func start(pos):
