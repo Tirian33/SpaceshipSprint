f25003013623e9995153b4e96938cb68933dbde0
KhoaNguyen <117057796+KoaHaz@users.noreply.github.com>
2024-04-16 02:37:49 -0500

    Update dynamic shop system

--- Shop/shop.gd
+++ Shop/shop.gd
@@ -4,6 +4,15 @@ extends CanvasLayer
 func _ready():
 	get_node("ShopAnimation").play("TransIn")
 	$Panel/Gold.text = "$ " + str(Global.gold)
+	Global.gold_update.connect(gold_update,0)
+
+func gold_update():
+	$Panel/Gold.text = "$ " + str(Global.gold)
 	
+
 func _on_close_button_pressed():
 	get_node("ShopAnimation").play("TransOut")
+
+#warning auto disappearing
+func _on_timer_timeout():
+	$Panel/Warning.visible = false
