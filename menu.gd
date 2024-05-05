be9cd42f86ec4b4b4d72b7d5ae3729c7e6720729
Matthew Lane <matthewglenn@protonmail.com>
2024-05-05 16:45:33 -0500

    Change reset to button to restart save button, set gold to 0 on save restart

--- menu.gd
+++ menu.gd
@@ -24,7 +24,7 @@ func _on_quit_button_pressed():
 
 func _on_new_save_button_pressed():
 	Global.new_save()
-	Global.gold = 10000    #for testing
+	Global.gold = 0
 	Global.gold_update.emit()
 	Global.status_changed.emit()
 	Global.write_save(Global.item_list)
